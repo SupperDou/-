2d6dc6977e3b214a648915ad6d3abe32bb6dceff
SupperDou <60504430+SupperDou@users.noreply.github.com>
2020-02-08 11:40:24 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,1 +1,14 @@
-<?phpheader("Access-Control-Allow-Origin:*");date_default_timezone_set("PRC");$result=file_get_contents("https://api.ooopn.com/yan/api.php");$arr=json_decode($result,true);if($arr['hitokoto']){header("Content-type: application/json;charset=UTF-8");//输出一句话时，改成 $arr['hitokoto']echojson_encode($arr,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);}else{header("Content-Type:text/html;charset=UTF-8");echo'请求错误，请重新尝试';}?>
+<?php
+header("Access-Control-Allow-Origin:*");
+date_default_timezone_set("PRC");
+$result = file_get_contents("https://api.ooopn.com/yan/api.php");
+$arr=json_decode($result,true);
+if ($arr['hitokoto']) {
+    header("Content-type: application/json;charset=UTF-8");
+    //输出一句话时，改成 $arr['hitokoto']
+    echo json_encode($arr,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+}else{
+    header("Content-Type:text/html;charset=UTF-8");
+    echo '请求错误，请重新尝试';
+}
+?>
